02bfc50ad0f6d087c92898a228ccd82e5be7e6a3
BilalHamaydi <bhamaydi@gmail.com>
2025-11-24 13:12:59 +0100

    update for render 9

--- WhatToDov1-fronetnd/src/components/TheWelcome.vue
+++ WhatToDov1-fronetnd/src/components/TheWelcome.vue
@@ -15,7 +15,7 @@ const loading = ref(false)
 const error = ref('')
 
 // Ersetze diese URL durch deine Render-Backend-URL!
-const apiUrl = 'https://whattodov1.onrender.com'
+const apiUrl = 'https://whattodov1.onrender.com/todos/'
 
 // 3. Laden der Todos beim Start
 onMounted(loadTodos)
